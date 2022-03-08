24069c509c9622c100213a31e0ad723d47f72916
Kyleah Justice <kikij432@gmail.com>
2022-03-08 16:54:50 -0700

    starting on the server.js page. adding everthing needing to be required, and adding the middleware and the port for my database

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
-const
+const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
+
+
+const PORT = process.env.PORT || 3306;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
