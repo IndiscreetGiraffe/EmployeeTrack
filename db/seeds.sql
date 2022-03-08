3e71382a2385b6edb5122e0355beb38324bc3f17
Kyleah Justice <kikij432@gmail.com>
2022-03-08 14:48:07 -0700

    adding the prepopulated database for employee roles, once again just used what was in the example on the challenge page

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,13 @@ VALUES
     ('Finance'),
     ('Egineering'),
     ('Legal');
+
+INSERT INTO employeeroles (title, salary, department_id)
+VALUES
+    ('Salesperson', '80000', '1'),
+    ('Accountant', '125000', '2'),
+    ('Account Manager', '160000', '2'),
+    ('Software Engineer', '120000','3'),
+    ('Lead Engineer', '150000', '3'),
+    ('Lawyer', '190000', '4'),
+    ('Legal Team Lead', '250000', '4');
