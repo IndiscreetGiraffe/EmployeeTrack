5b11dbabaa024db875e86188e36e44d008f0fca2
Kyleah Justice <kikij432@gmail.com>
2022-03-08 15:20:39 -0700

    added the function to prompt the choices for the database

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const { express } = require('express');
 const inquirer = require('inquirer');
 const db = require('../db/connection');
 const cTable = require('console.table');
+const { isAsyncFunction } = require('util/types');
 
 let departments = [];
 let roles = [];
@@ -10,3 +11,15 @@ let updatedRole = [];
 
 console.log("--------------Employee Tracker--------------------");
 
+async function startQuestions() {
+    const question = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'trackerAction',
+            message: "What would you like to do today?",
+            choices: ['View all departments', 'View all employees', 'View all roles', 'Add a department', 'Add an employee', 'Add a role', 'Update an employee role', 'Quit']
+        }
+    ])
+
+    
+}
