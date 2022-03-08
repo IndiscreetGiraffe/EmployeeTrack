159087112ed13eaf7053bc1732fa63eaa1e52699
Kyleah Justice <kikij432@gmail.com>
2022-03-08 16:57:31 -0700

    using the function code I was using in the index.js page, I added a get to beiw all departments

--- server.js
+++ server.js
@@ -10,3 +10,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: "Error" });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
