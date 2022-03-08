1bc62a211044d44b681f2f5565df4018fdc84736
Kyleah Justice <kikij432@gmail.com>
2022-03-08 14:17:44 -0700

    completed the connection page to connect to mysql

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require ('mysql2');
+
+const db = mysql.createConnection( 
+    {
+        host: 'localhost',
+
+        user:'root',
+        password: 'Octavia6114!',
+        database: 'employee_tracker'
+    },
+    console.log('Connected to the Employee Database.')
+);
+
+module.exports = db;
