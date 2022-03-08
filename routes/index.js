132859a984731dd14173431ca7b83dc8b40a8c67
Kyleah Justice <kikij432@gmail.com>
2022-03-08 15:14:29 -0700

    i'm starting on the code to start the questions to view, add, or update any roles, departments, or employees

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const { express } = require('express');
+const inquirer = require('inquirer');
+const db = require('../db/connection');
+const cTable = require('console.table');
+
+let departments = [];
+let roles = [];
+let employees = [];
+let updatedRole = [];
+
+console.log("--------------Employee Tracker--------------------");
+
