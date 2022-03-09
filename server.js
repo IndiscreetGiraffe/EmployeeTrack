906c024620fc390753256228dad32608eb600930
Kyleah Justice <kikij432@gmail.com>
2022-03-08 21:12:35 -0700

    added the final code of the page to resond to any other requests not specified

--- server.js
+++ server.js
@@ -144,3 +144,10 @@ app.put('/api/employee/:id', (req, res) => {
     });
 });
 
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
